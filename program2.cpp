9cbed9c9ae13f61211c14f46add73406b9110f4d
Mehebul02 <mehebul2123@gmail.com>
2026-02-02 19:58:32 +0600

    total sum

--- program2.cpp
+++ program2.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-    int num1;
-    int num2;
-
+    int num1, num2;
     num1 =10;
     num2 = 33;
 
-    cout << num1 << endl;  
-    cout << num2 << endl;  
+    int total =num1+ num2;
+
+    cout << total << endl;  
+    
     getch(); 
     return 0;                 
   
